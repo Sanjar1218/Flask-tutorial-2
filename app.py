42e32eec75e2a5102b173b393859dcd711fa8ab5
sanjar <sanjar@pop-os.localdomain>
2020-12-08 16:38:54 +0500

    flash

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -16,9 +16,11 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session['user'] = user
+        flash("login successful")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash("already log in")
             return redirect(url_for('user'))
         return render_template("login.html")
   
@@ -27,14 +29,20 @@ def login():
 def user():
     if 'user' in session:
         user = session['user']
-        return f"<h1>{user}</h1>"
+        return render_template("user.html", user=user)
     else:
+        flash(" you are not logged in")
         return redirect(url_for('login'))
-
+ 
 @app.route("/logout")
 def logout():
+    flash("you have been logged out", "info")
     session.pop("user", None)
+    
     return redirect(url_for('login'))
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
