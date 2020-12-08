2596ee0405d6321aea0d147fa24758810537ab90
sanjar <sanjar@pop-os.localdomain>
2020-12-08 19:50:07 +0500

    sqlalchemy

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "what"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    _id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.String(100))
+    email = db.Column('email', db.String(100))
+    
+    def __inti__(self, name, email):
+        self.name = name
+        self.email = email
+    
 
 @app.route('/')
 def home():
@@ -23,13 +38,23 @@ def login():
             flash("already log in")
             return redirect(url_for('user'))
         return render_template("login.html")
-  
 
-@app.route("/user")
+
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if 'user' in session:
         user = session['user']
-        return render_template("user.html", user=user)
+        
+        if request.method == "POST":
+            email = request.form['email']
+            session['email'] = email
+            flash('email was saved')
+        else:
+            if 'email' in session:
+                email = session['email']
+            
+        return render_template("user.html", email=email)
     else:
         flash(" you are not logged in")
         return redirect(url_for('login'))
@@ -38,11 +63,12 @@ def user():
 def logout():
     flash("you have been logged out", "info")
     session.pop("user", None)
-    
+    session.pop('email', None)
     return redirect(url_for('login'))
 
 
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
