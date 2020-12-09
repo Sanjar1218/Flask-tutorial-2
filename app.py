b34115c6db25b1deafda292c77662ce9d22e7ea0
sanjar <sanjar@pop-os.localdomain>
2020-12-09 11:02:30 +0500

    sqlalchemy

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 
 db = SQLAlchemy(app)
 
-class Users(db.Model):
+class users(db.Model):
     _id = db.Column('id', db.Integer, primary_key=True)
     name = db.Column('name', db.String(100))
     email = db.Column('email', db.String(100))
     
-    def __inti__(self, name, email):
+    def __init__(self, name, email):
         self.name = name
         self.email = email
     
@@ -25,12 +25,25 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/view')
+def view():
+    return render_template('view.html',values=users.query.all())
+
 @app.route('/login', methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         session.permanent = True
         user = request.form["nm"]
         session['user'] = user
+        
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session['email'] = found_user.email
+        else:
+            usr = users(user, '')
+            db.session.add(usr)
+            db.session.commit()
+        
         flash("login successful")
         return redirect(url_for("user"))
     else:
@@ -49,6 +62,8 @@ def user():
         if request.method == "POST":
             email = request.form['email']
             session['email'] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email 
             flash('email was saved')
         else:
             if 'email' in session:
